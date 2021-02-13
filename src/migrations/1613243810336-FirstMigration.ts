b31874a354e13cf6d2464535b2a0dab67d125e0d
Peter Ayeni <ayenicology@gmail.com>
2021-02-13 19:38:07 +0000

    chore: setup migrations

---
+++ src/migrations/1613243810336-FirstMigration.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class FirstMigration1613243810336 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
